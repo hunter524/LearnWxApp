55a75d23259591976cfb55350afd8beea3b6eed9
hunter <guyuesh2@gmail.com>
2018-08-23 14:57:25 +0800

    Init

---
+++ src/pages/index/index.js
@@ -0,0 +1,30 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import  './index.scss'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text className='text-style'>Hello!</Text>
+      </View>
+    )
+  }
+}
+
